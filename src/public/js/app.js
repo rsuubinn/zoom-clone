8ab4eac2af25849e40ceb21a2812fe7546b9d772
rsuubinn <rsuubinn@gmail.com>
2022-05-29 20:24:27 +0900

    #2.3 Recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,12 +1,14 @@
 const socket = io();
 const welcome = document.getElementById("welcome");
 
+function backendDone(msg) {
+  console.log(`The backend says: ${msg}`);
+}
+
 const handleRoomSubmit = (event) => {
   event.preventDefault();
   const input = welcome.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () =>
-    console.log("Server is done!")
-  );
+  socket.emit("enter_room", input.value, backendDone);
   input.value = "";
 };
 
--- src/server.js
+++ src/server.js
@@ -15,10 +15,10 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (message, done) => {
-    console.log(message);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
     setTimeout(() => {
-      done();
+      done("hello from the backend");
     }, 5000);
   });
 });
