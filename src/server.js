fee754c4fe577858e698d5104545c53b410d49cb
rsuubinn <rsuubinn@gmail.com>
2022-08-08 23:47:16 +0900

    #1.0 Introduction

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import http from "http";
-import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -7,11 +5,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
-
-const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-httpServer.listen(3000, handleListen);
+app.listen(3000, handleListen);
