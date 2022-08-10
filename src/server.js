a89ca0b3b688c468358ccd48b4500369a01b0987
rsuubinn <rsuubinn@gmail.com>
2022-08-10 20:07:44 +0900

    #1.2 WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -9,4 +11,8 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
